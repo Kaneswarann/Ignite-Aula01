7daa860de46bafa94414b2bf1c7944ef45ef45de
Rebeka <rebekamaisa@gmail.com>
2023-08-28 20:11:51 -0300

    usando useState na construção do formulário

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,36 @@ import { Post } from './components/Post'
 import './global.css'
 import sytles from "./components/App.module.css";
 
-
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/kaneswarann.png',
+      name: 'Rebeka Palmeira',
+      role: 'Web Developer'
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa 👋'},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type: 'link', content: 'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2022-05-03 20:00:00'),
+  },
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/JuanTavares.png',
+      name: 'Juan Carlos Tavares',
+      role: 'IT Analyst'
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa 👋'},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type: 'link', content: 'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2022-05-03 20:00:00'),
+  },
+]
 
 export function App() {
 
@@ -15,7 +44,14 @@ export function App() {
       <div className={sytles.wrapper}> 
         <Sidebar />
         <main>
-        <Post />
+        {posts.map(post => {
+          return (
+          <Post 
+            author={post.author}
+            content={post.content}
+            publishedAt={post.publishedAt}
+          />)
+        })}
         </main>
       </div>
 
