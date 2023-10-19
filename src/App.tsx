9175db21bd9b49598cecb296b3e6086ceba501c0
Rebeka <rebekamaisa@gmail.com>
2023-10-19 03:08:48 -0300

    Usando spread operator e adicionando tipagem

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 
 import { Header } from './components/Header'
-import { Sidebar } from './components/Sidebar';
-import { Post } from './components/Post'
+import { Sidebar } from './components/Sidebar'
+import { Post, PostType } from './components/Post'
 import './global.css'
 import sytles from "./components/App.module.css";
 
-const posts = [
+const posts: PostType[] = [
   {
     id: 1,
     author: {
@@ -48,9 +48,7 @@ export function App() {
           return (
           <Post 
             key={post.id}
-            author={post.author}
-            content={post.content}
-            publishedAt={post.publishedAt}
+            post={post}
           />)
         })}
         </main>
